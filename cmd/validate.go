ac1f1600fa47552fb1992539af00acd1c487fc83
RMcDOttawa <richard@themcdonalds.net>
2024-06-09 10:50:09 -0400

    Implement validate subcommand

--- cmd/validate.go
+++ cmd/validate.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -14,21 +13,67 @@ var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "Validate the config file",
 	Long:  `Validates the config file and displays all the settings.  No capture is performed.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("validate called")
-	},
+	Run:   runValidateCommand,
+}
+
+func runValidateCommand(_ *cobra.Command, _ []string) {
+	fmt.Println("Validating and displaying all config settings:")
+
+	//	Global settings
+	fmt.Println("Global settings")
+	fmt.Printf("   Verbosity: %d\n", Settings.Verbosity)
+	fmt.Printf("   Debug: %t\n", Settings.Debug)
+	fmt.Printf("   State File Path: %s\n", Settings.StateFile)
+
+	//	Server settings
+	fmt.Println("Server settings")
+	fmt.Printf("   Address: %s\n", Settings.Server.Address)
+	fmt.Printf("   Port: %d\n", Settings.Server.Port)
+
+	//	Start time
+	fmt.Println("Delayed Start settings")
+	fmt.Printf("   Delay: %t\n", Settings.Start.Delay)
+	fmt.Printf("   Day: %s\n", Settings.Start.Day)
+	fmt.Printf("   Time: %s\n", Settings.Start.Time)
+	delay, start, err := Settings.ParseStart()
+	if err != nil {
+		fmt.Printf("Error parsing start settings: %s\n", err)
+	}
+	fmt.Printf("   Converted to: %t, %v\n", delay, start)
+
+	//	Cooling info
+	fmt.Println("Cooling settings")
+	fmt.Printf("   Use cooler: %t\n", Settings.Cooling.UseCooler)
+	fmt.Printf("   Cool to: %g degrees\n", Settings.Cooling.CoolTo)
+	fmt.Printf("   Start tolerance: %g degrees\n", Settings.Cooling.CoolStartTol)
+	fmt.Printf("   Wait maximum: %d minutes\n", Settings.Cooling.CoolWaitMinutes)
+	fmt.Printf("   Abort if cooling outside tolerance: %t\n", Settings.Cooling.AbortOnCooling)
+	fmt.Printf("   Abort tolerance: %g degrees\n", Settings.Cooling.CoolAbortTol)
+
+	//	Bias Frames
+	fmt.Println("Bias Frames")
+	bf := Settings.GetBiasSets()
+	if len(bf) == 0 {
+		fmt.Println("   No bias sets")
+	} else {
+		for _, bias := range bf {
+			fmt.Printf("    %d frames binned at %d\n", bias.Frames, bias.Binning)
+		}
+	}
+
+	//	Dark Frames
+	fmt.Println("Dark Frames")
+	df := Settings.GetDarkSets()
+	if len(df) == 0 {
+		fmt.Println("   No dark sets")
+	} else {
+		for _, dark := range df {
+			fmt.Printf("    %d frames of %d seconds binned at %d\n",
+				dark.Frames, dark.Seconds, dark.Binning)
+		}
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(validateCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// validateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// validateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
